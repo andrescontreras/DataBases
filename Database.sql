38f05309f2e693fc1c3a7b08dc40fd88e21f1341
Alejandro Castro <alejandrocastro9b@hotmail.com>
2016-08-10 14:38:39 -0500

    Esqueleto general

--- Database.sql
+++ Database.sql
@@ -1,1 +1,28 @@
+/*
+Proyecto de Bases de datos 2016-30
+Realizado por:
+  Pablo Ariza
+  Alejandro Castro
+  Andres Contreras
+  Santiago Salamanca
+*/
 
+/* Creacion de tablas (No olvidar DROP TABLE)*/
+
+
+/* Insercion de datos en las tablas */
+
+
+/* Punto 1: Monedas por continente */
+
+
+
+/* Punto 2: Moneda con tasas de cambio en todas las monedas */
+
+
+
+/* Punto 3: Monedas que faltan tasas de cambio respecto a las demas monedas */
+
+
+
+/* Punto 4: Porcentaje de aumento o disminucion de la tasa de cambio de cada combinacion */
