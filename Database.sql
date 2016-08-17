0a27cd7a5e35431dcd12c06da8a2d1ce1cfa3314
Alejandro Castro <alejandrocastro9b@hotmail.com>
2016-08-17 18:07:19 -0500

    Creacion tablas

--- Database.sql
+++ Database.sql
@@ -9,7 +9,43 @@ Realizado por:
 */
 
 /* Creacion de tablas (No olvidar DROP TABLE)*/
-
+DROP TABLE tasas;
+DROP TABLE monedaXpais;
+DROP TABLE moneda;
+DROP TABLE pais;
+DROP TABLE continente;
+CREATE TABLE continente(
+  cod_continente numeric (3,0) PRIMARY KEY,
+  descripcion varchar (50) NOT NULL UNIQUE
+);
+CREATE TABLE pais (
+  cod_continente numeric (3,0),
+  cod_pais numeric (3,0),
+  nombre varchar (20) NOT NULL,
+  PRIMARY KEY (cod_continente,cod_pais),
+  FOREIGN KEY (cod_continente) REFERENCES continente(cod_continente)
+);
+CREATE TABLE moneda(
+  id_moneda numeric (3,0) PRIMARY KEY,
+  nombre varchar (10) NOT NULL
+);
+CREATE TABLE monedaXpais(
+  id_moneda numeric (3,0),
+  cod_continente numeric (3,0),
+  id_pais numeric (3,0),
+  PRIMARY KEY (id_moneda,cod_continente,id_pais),
+  FOREIGN KEY (id_moneda) REFERENCES moneda(id_moneda),
+  FOREIGN KEY (cod_continente,id_pais) REFERENCES pais(cod_continente,cod_pais)
+);
+CREATE TABLE tasas(
+  id_moneda_origen numeric (3,0),
+  id_moneda_destino numeric (3,0),
+  fecha date,
+  tasa_cambio numeric (5,2),
+  PRIMARY KEY (id_moneda_origen,id_moneda_destino,fecha),
+  FOREIGN KEY (id_moneda_origen) REFERENCES moneda(id_moneda),
+  FOREIGN KEY (id_moneda_destino) REFERENCES moneda(id_moneda)
+);
 
 /* Insercion de datos en las tablas */
 
